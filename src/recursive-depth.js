2ff2146c59547f67f955e77f1e11197595742d18
Artemido4ka <korenko.artem9@gmail.com>
2021-03-29 21:22:20 +0300

    feat:add

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,24 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-   d = 0;
-   counter = 0;
+ 
   calculateDepth(arr) {
+    let counter = 1;
     for(let i = 0;i<arr.length;i++){
       if(Array.isArray(arr[i])==true){
-        this.d++;
-       this.calculateDepth(arr[i]);
+        counter = Math.max(counter, 1 + this.calculateDepth(arr[i]));
        }
-
-       if(this.d>this.counter){
-        this.counter = this.d;
-        this.d = 0;   
-       }
-
-
-    }
-    let result =  this.counter;
-    return result+1;
+      }
+    return counter;
   }
 
 }
